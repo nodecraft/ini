04239d956c1b93bc57c4d1306f3d7584cfafc1b0
James Ross <james@jross.me>
2019-10-08 18:46:10 +0100

    chore: stylististic and modernisation changes
    - modernises some code
    - moves to const/let instead of var
    - switches to ESLint
    - improves code readbility
    - tweaks EOL detection to use `os` library rather than manually

--- ini.js
+++ ini.js
@@ -1,194 +1,201 @@
-exports.parse = exports.decode = decode
+'use strict';
+exports.parse = exports.decode = decode;
 
-exports.stringify = exports.encode = encode
+exports.stringify = exports.encode = encode;
 
-exports.safe = safe
-exports.unsafe = unsafe
+exports.safe = safe;
+exports.unsafe = unsafe;
 
-var eol = typeof process !== 'undefined' &&
-  process.platform === 'win32' ? '\r\n' : '\n'
+const eol = require('os').EOL;
 
-function encode (obj, opt) {
-  var children = []
-  var out = ''
+function encode(obj, opt){
+	const children = [];
+	let out = '';
 
-  if (typeof opt === 'string') {
-    opt = {
-      section: opt,
-      whitespace: false
-    }
-  } else {
-    opt = opt || {}
-    opt.whitespace = opt.whitespace === true
-  }
+	// opt.section is passed in recursively. If passed in on top-level, it'll affect both the top-level ini keys, and any children
+	if(typeof opt === 'string'){
+		opt = {
+			section: opt,
+			whitespace: false
+		};
+	}else{
+		opt = opt || {};
+		opt.whitespace = opt.whitespace === true;
+	}
+	const separator = opt.whitespace ? ' = ' : '=';
 
-  var separator = opt.whitespace ? ' = ' : '='
+	for(const [key, val] of Object.entries(obj)){
+		if(val && Array.isArray(val)){
+			val.forEach(function(item){
+				out += safe(key + '[]') + separator + safe(item) + eol;
+			});
+		}else if(val && typeof val === 'object'){
+			children.push(key);
+		}else{
+			out += safe(key) + separator + safe(val) + eol;
+		}
+	}
 
-  Object.keys(obj).forEach(function (k, _, __) {
-    var val = obj[k]
-    if (val && Array.isArray(val)) {
-      val.forEach(function (item) {
-        out += safe(k + '[]') + separator + safe(item) + '\n'
-      })
-    } else if (val && typeof val === 'object') {
-      children.push(k)
-    } else {
-      out += safe(k) + separator + safe(val) + eol
-    }
-  })
+	if(opt.section && out.length){
+		out = '[' + safe(opt.section) + ']' + eol + out;
+	}
 
-  if (opt.section && out.length) {
-    out = '[' + safe(opt.section) + ']' + eol + out
-  }
+	children.forEach(function(key){
+		const parsedSection = dotSplit(key).join('\\.');
+		const section = (opt.section ? opt.section + '.' : '') + parsedSection;
+		const child = encode(obj[key], {
+			section: section,
+			whitespace: opt.whitespace
+		});
+		if(out.length && child.length){
+			out += eol;
+		}
+		out += child;
+	});
 
-  children.forEach(function (k, _, __) {
-    var nk = dotSplit(k).join('\\.')
-    var section = (opt.section ? opt.section + '.' : '') + nk
-    var child = encode(obj[k], {
-      section: section,
-      whitespace: opt.whitespace
-    })
-    if (out.length && child.length) {
-      out += eol
-    }
-    out += child
-  })
-
-  return out
+	return out;
 }
 
-function dotSplit (str) {
-  return str.replace(/\1/g, '\u0002LITERAL\\1LITERAL\u0002')
-    .replace(/\\\./g, '\u0001')
-    .split(/\./).map(function (part) {
-      return part.replace(/\1/g, '\\.')
-      .replace(/\2LITERAL\\1LITERAL\2/g, '\u0001')
-    })
+function dotSplit(str){
+	return str.replace(/\1/g, '\u0002LITERAL\\1LITERAL\u0002')
+		.replace(/\\\./g, '\u0001')
+		.split(/\./).map(function(part){
+			return part.replace(/\1/g, '\\.')
+				.replace(/\2LITERAL\\1LITERAL\2/g, '\u0001');
+		});
 }
 
-function decode (str) {
-  var out = {}
-  var p = out
-  var section = null
-  //          section     |key      = value
-  var re = /^\[([^\]]*)\]$|^([^=]+)(=(.*))?$/i
-  var lines = str.split(/[\r\n]+/g)
+function decode(str){
+	const out = {};
+	let ref = out;
+	let section = null;
+	//          section       |key        = value
+	const re = /^\[([^\]]*)\]$|^([^=]+)(?:=(.*))?$/i;
+	const lines = str.split(/[\r\n]+/g);
+	const commentMatch = /^\s*[;#]/;
+	lines.forEach(function(line){
+		if(!line || line.match(commentMatch)){ return; }
+		const match = line.match(re);
+		if(!match){ return; }
+		if(match[1] !== undefined){
+			section = unsafe(match[1]);
+			ref = out[section] = out[section] || {};
+			return;
+		}
+		let key = unsafe(match[2]);
+		let value = match[3] ? unsafe(match[3]) : true;
+		switch(value){
+			case 'true':
+			case 'false':
+			case 'null': value = JSON.parse(value);
+		}
 
-  lines.forEach(function (line, _, __) {
-    if (!line || line.match(/^\s*[;#]/)) return
-    var match = line.match(re)
-    if (!match) return
-    if (match[1] !== undefined) {
-      section = unsafe(match[1])
-      p = out[section] = out[section] || {}
-      return
-    }
-    var key = unsafe(match[2])
-    var value = match[3] ? unsafe(match[4]) : true
-    switch (value) {
-      case 'true':
-      case 'false':
-      case 'null': value = JSON.parse(value)
-    }
+		// Convert keys with '[]' suffix to an array
+		if(key.length > 2 && key.slice(-2) === '[]'){
+			key = key.substring(0, key.length - 2);
+			if(!ref[key]){
+				ref[key] = [];
+			}else if(!Array.isArray(ref[key])){
+				ref[key] = [ref[key]];
+			}
+		}
 
-    // Convert keys with '[]' suffix to an array
-    if (key.length > 2 && key.slice(-2) === '[]') {
-      key = key.substring(0, key.length - 2)
-      if (!p[key]) {
-        p[key] = []
-      } else if (!Array.isArray(p[key])) {
-        p[key] = [p[key]]
-      }
-    }
+		// safeguard against resetting a previously defined
+		// array by accidentally forgetting the brackets
+		if(Array.isArray(ref[key])){
+			ref[key].push(value);
+		}else{
+			ref[key] = value;
+		}
+	});
 
-    // safeguard against resetting a previously defined
-    // array by accidentally forgetting the brackets
-    if (Array.isArray(p[key])) {
-      p[key].push(value)
-    } else {
-      p[key] = value
-    }
-  })
+	// {a:{y:1},"a.b":{x:2}} --> {a:{y:1,b:{x:2}}}
+	// use a filter to return the keys that have to be deleted.
+	Object.keys(out).filter(function(key){
+		if(!out[key] || typeof out[key] !== 'object' || Array.isArray(out[key])){
+			return false;
+		}
+		// see if the parent section is also an object.
+		// if so, add it to that, and mark this one for deletion
+		const parts = dotSplit(key);
+		let p = out;
+		const lastKey = parts.pop();
+		const unescapedLastKey = lastKey.replace(/\\\./g, '.');
+		parts.forEach(function(part){
+			if(!p[part] || typeof p[part] !== 'object'){
+				p[part] = {};
+			}
+			p = p[part];
+		});
+		if(p === out && unescapedLastKey === lastKey){
+			return false;
+		}
+		p[unescapedLastKey] = out[key];
+		return true;
+	}).forEach(function(del){
+		delete out[del];
+	});
 
-  // {a:{y:1},"a.b":{x:2}} --> {a:{y:1,b:{x:2}}}
-  // use a filter to return the keys that have to be deleted.
-  Object.keys(out).filter(function (k, _, __) {
-    if (!out[k] ||
-      typeof out[k] !== 'object' ||
-      Array.isArray(out[k])) {
-      return false
-    }
-    // see if the parent section is also an object.
-    // if so, add it to that, and mark this one for deletion
-    var parts = dotSplit(k)
-    var p = out
-    var l = parts.pop()
-    var nl = l.replace(/\\\./g, '.')
-    parts.forEach(function (part, _, __) {
-      if (!p[part] || typeof p[part] !== 'object') p[part] = {}
-      p = p[part]
-    })
-    if (p === out && nl === l) {
-      return false
-    }
-    p[nl] = out[k]
-    return true
-  }).forEach(function (del, _, __) {
-    delete out[del]
-  })
-
-  return out
+	return out;
 }
 
-function isQuoted (val) {
-  return (val.charAt(0) === '"' && val.slice(-1) === '"') ||
-    (val.charAt(0) === "'" && val.slice(-1) === "'")
+// determines if string is encased in quotes
+function isQuoted(val){
+	return (val.startsWith('"') && val.endsWith('"')) || (val.startsWith("'") && val.endsWith("'"));
 }
 
-function safe (val) {
-  return (typeof val !== 'string' ||
-    val.match(/[=\r\n]/) ||
-    val.match(/^\[/) ||
-    (val.length > 1 &&
-     isQuoted(val)) ||
-    val !== val.trim())
-      ? JSON.stringify(val)
-      : val.replace(/;/g, '\\;').replace(/#/g, '\\#')
+// escapes the string val such that it is safe to be used as a key or value in an ini-file. Basically escapes quotes
+function safe(val){
+	// all kinds of values and keys
+	if(typeof val !== 'string' || val.match(/[=\r\n]/) || val.match(/^\[/) || (val.length > 1 && isQuoted(val)) || val !== val.trim()){
+		return JSON.stringify(val);
+	}
+	// comments
+	return val.replace(/;/g, '\\;').replace(/#/g, '\\#');
 }
 
-function unsafe (val, doUnesc) {
-  val = (val || '').trim()
-  if (isQuoted(val)) {
-    // remove the single quotes before calling JSON.parse
-    if (val.charAt(0) === "'") {
-      val = val.substr(1, val.length - 2)
-    }
-    try { val = JSON.parse(val) } catch (_) {}
-  } else {
-    // walk the val to find the first not-escaped ; character
-    var esc = false
-    var unesc = ''
-    for (var i = 0, l = val.length; i < l; i++) {
-      var c = val.charAt(i)
-      if (esc) {
-        if ('\\;#'.indexOf(c) !== -1) {
-          unesc += c
-        } else {
-          unesc += '\\' + c
-        }
-        esc = false
-      } else if (';#'.indexOf(c) !== -1) {
-        break
-      } else if (c === '\\') {
-        esc = true
-      } else {
-        unesc += c
-      }
-    }
-    if (esc) {
-      unesc += '\\'
-    }
-    return unesc.trim()
-  }
-  return val
+// unescapes the string val
+function unsafe(val){
+	const escapableChars = '\\;#',
+		commentChars = ';#';
+
+	val = (val || '').trim();
+	if(isQuoted(val)){
+		// remove the single quotes before calling JSON.parse
+		if(val.charAt(0) === "'"){
+			val = val.substr(1, val.length - 2);
+		}
+		try{
+			val = JSON.parse(val);
+		}catch(e){
+			// we tried :(
+		}
+		return val;
+	}
+	// walk the val to find the first not-escaped ; character
+	let isEscaping = false;
+	let escapedVal = '';
+	for(let i = 0, l = val.length; i < l; i++){
+		const c = val.charAt(i);
+		if(isEscaping){
+			// check if this character is an escapable character like \ or ; or #
+			if(escapableChars.indexOf(c) !== -1){
+				escapedVal += c;
+			}else{
+				escapedVal += '\\' + c;
+			}
+			isEscaping = false;
+		}else if(commentChars.indexOf(c) !== -1){
+			break;
+		}else if(c === '\\'){
+			isEscaping = true;
+		}else{
+			escapedVal += c;
+		}
+	}
+	// we're still escaping - something isn't right. Close out with an escaped escape char
+	if(isEscaping){
+		escapedVal += '\\';
+	}
+	return escapedVal.trim();
 }
--- test/foo.js
+++ test/foo.js
@@ -1,12 +1,12 @@
-var i = require("../")
-  , tap = require("tap")
-  , test = tap.test
-  , fs = require("fs")
-  , path = require("path")
-  , fixture = path.resolve(__dirname, "./fixtures/foo.ini")
-  , data = fs.readFileSync(fixture, "utf8")
-  , d
-  , expectE = 'o=p\n'
+var i = require("../"),
+	tap = require("tap"),
+	test = tap.test,
+	fs = require("fs"),
+	path = require("path"),
+	fixture = path.resolve(__dirname, "./fixtures/foo.ini"),
+	data = fs.readFileSync(fixture, "utf8"),
+	d,
+	expectE = 'o=p\n'
             + 'a with spaces=b  c\n'
             + '" xa  n          p "="\\"\\r\\nyoyoyo\\r\\r\\n"\n'
             + '"[disturbing]"=hey you never know\n'
@@ -28,80 +28,80 @@ var i = require("../")
             + ' \\"a val\\", b: { c: { e: \\"this [value]'
             + '\\" } } } }\\""\n"[]"=a square?\n'
             + 'cr[]=four\ncr[]=eight\n\n'
-            +'[a.b.c]\ne=1\n'
+            + '[a.b.c]\ne=1\n'
             + 'j=2\n\n[x\\.y\\.z]\nx.y.z=xyz\n\n'
             + '[x\\.y\\.z.a\\.b\\.c]\na.b.c=abc\n'
             + 'nocomment=this\\; this is not a comment\n'
-            + 'noHashComment=this\\# this is not a comment\n'
-  , expectD =
+            + 'noHashComment=this\\# this is not a comment\n',
+	expectD =
     { o: 'p',
-      'a with spaces': 'b  c',
-      " xa  n          p ":'"\r\nyoyoyo\r\r\n',
-      '[disturbing]': 'hey you never know',
-      's': 'something',
-      's1' : '\"something\'',
-      's2': 'something else',
-      'zr': ['deedee'],
-      'ar': ['one', 'three', 'this is included'],
-      'br': 'warm',
-      'eq': 'eq=eq',
-      a:
+    	'a with spaces': 'b  c',
+    	" xa  n          p ": '"\r\nyoyoyo\r\r\n',
+    	'[disturbing]': 'hey you never know',
+    	's': 'something',
+    	's1': '\"something\'',
+    	's2': 'something else',
+    	'zr': ['deedee'],
+    	'ar': ['one', 'three', 'this is included'],
+    	'br': 'warm',
+    	'eq': 'eq=eq',
+    	a:
        { av: 'a val',
-         e: '{ o: p, a: { av: a val, b: { c: { e: "this [value]" } } } }',
-         j: '"{ o: "p", a: { av: "a val", b: { c: { e: "this [value]" } } } }"',
-         "[]": "a square?",
-         cr: ['four', 'eight'],
-         b: { c: { e: '1', j: '2' } } },
-      'x.y.z': {
-        'x.y.z': 'xyz',
-        'a.b.c': {
-          'a.b.c': 'abc',
-          'nocomment': 'this\; this is not a comment',
-          noHashComment: 'this\# this is not a comment'
-        }
-      }
-    }
-  , expectF = '[prefix.log]\n'
+       	e: '{ o: p, a: { av: a val, b: { c: { e: "this [value]" } } } }',
+       	j: '"{ o: "p", a: { av: "a val", b: { c: { e: "this [value]" } } } }"',
+       	"[]": "a square?",
+       	cr: ['four', 'eight'],
+       	b: { c: { e: '1', j: '2' } } },
+    	'x.y.z': {
+    		'x.y.z': 'xyz',
+    		'a.b.c': {
+    			'a.b.c': 'abc',
+    			'nocomment': 'this\; this is not a comment',
+    			noHashComment: 'this\# this is not a comment'
+    		}
+    	}
+    },
+	expectF = '[prefix.log]\n'
             + 'type=file\n\n'
             + '[prefix.log.level]\n'
             + 'label=debug\n'
-            + 'value=10\n'
-  , expectG = '[log]\n'
+            + 'value=10\n',
+	expectG = '[log]\n'
             + 'type = file\n\n'
             + '[log.level]\n'
             + 'label = debug\n'
-            + 'value = 10\n'
+            + 'value = 10\n';
 
-test("decode from file", function (t) {
-  var d = i.decode(data)
-  t.deepEqual(d, expectD)
-  t.end()
-})
+test("decode from file", function(t){
+	var d = i.decode(data);
+	t.deepEqual(d, expectD);
+	t.end();
+});
 
-test("encode from data", function (t) {
-  var e = i.encode(expectD)
-  t.deepEqual(e, expectE)
+test("encode from data", function(t){
+	var e = i.encode(expectD);
+	t.deepEqual(e, expectE);
 
-  var obj = {log: { type:'file', level: {label:'debug', value:10} } }
-  e = i.encode(obj)
-  t.notEqual(e.slice(0, 1), '\n', 'Never a blank first line')
-  t.notEqual(e.slice(-2), '\n\n', 'Never a blank final line')
+	var obj = {log: { type: 'file', level: {label: 'debug', value: 10} } };
+	e = i.encode(obj);
+	t.notEqual(e.slice(0, 1), '\n', 'Never a blank first line');
+	t.notEqual(e.slice(-2), '\n\n', 'Never a blank final line');
 
-  t.end()
-})
+	t.end();
+});
 
-test("encode with option", function (t) {
-  var obj = {log: { type:'file', level: {label:'debug', value:10} } }
-  e = i.encode(obj, {section: 'prefix'})
+test("encode with option", function(t){
+	var obj = {log: { type: 'file', level: {label: 'debug', value: 10} } };
+	e = i.encode(obj, {section: 'prefix'});
 
-  t.equal(e, expectF)
-  t.end()
-})
+	t.equal(e, expectF);
+	t.end();
+});
 
-test("encode with whitespace", function (t) {
-  var obj = {log: { type:'file', level: {label:'debug', value:10} } }
-  e = i.encode(obj, {whitespace: true})
+test("encode with whitespace", function(t){
+	var obj = {log: { type: 'file', level: {label: 'debug', value: 10} } };
+	e = i.encode(obj, {whitespace: true});
 
-  t.equal(e, expectG)
-  t.end()
-})
+	t.equal(e, expectG);
+	t.end();
+});
